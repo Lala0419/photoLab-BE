3765bb773d6b3774758da50f7c1fac9fea5e0e74
Nanaya <nanayamiki@gmail.com>
2023-09-21 20:08:25 -0700

    change ssl value

--- src/db/index.js
+++ src/db/index.js
@@ -1,16 +1,16 @@
 const pg = require("pg");
 
 const client = new pg.Client({
-  host: process.env.PGHOST,
-  name: process.env.PGDATABASE,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
-  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
+	host: process.env.PGHOST,
+	name: process.env.PGDATABASE,
+	user: process.env.PGUSER,
+	password: process.env.PGPASSWORD,
+	port: process.env.PGPORT,
+	ssl: true,
 });
 
 client
-  .connect()
-  .catch(e => console.log(`Error connecting to Postgres server:\n${e}`));
+	.connect()
+	.catch((e) => console.log(`Error connecting to Postgres server:\n${e}`));
 
 module.exports = client;
